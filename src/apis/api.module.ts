7117be148045693ccbd57316b6a6e3fd188c2f11
dobafullstack <doba.fullstack@gmail.com>
2023-06-15 15:30:51 +0700

    fix: fix file api module

--- src/apis/api.module.ts
+++ src/apis/api.module.ts
@@ -3,15 +3,8 @@ import { AdminModule } from './admin/admin.module';
 import { AuthModule } from './auth/auth.module';
 import { ChatModule } from './chat/chat.module';
 import { UploadModule } from './upload/upload.module';
-import { TestModule } from './test/test.module';
-import { TestTwoWordModule } from './test-two-word/test-two-word.module';
-import { TestTwoWordModule } from './test-two-word/test-two-word.module';
-import { TestModule } from './test/test.module';
-import { TestModule } from './test/test.module';
-import { TestModule } from './test/test.module';
-import { TestModule } from './test/test.module';
 
 @Module({
-	imports: [AdminModule, AuthModule, UploadModule, ChatModule, TestModule, TestTwoWordModule]
+	imports: [AdminModule, AuthModule, UploadModule, ChatModule]
 })
 export class ApiModule {}
